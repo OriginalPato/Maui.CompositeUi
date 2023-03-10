ee8fb72af83f5812038150993e0137391c6f6e51
James <jamesellisdev@outlook.com>
2023-03-10 21:49:22 +0000

    Initial Commit

---
+++ IRemoteDependencyResolver.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Maui.CompositeUi;
+
+public interface IRemoteDependencyResolver
+{
+    T Resolve<T>() where T : class, new();
+    TService Resolve<TService, [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TImplementation>() 
+        where TService : class 
+        where TImplementation : class, TService;
+}
